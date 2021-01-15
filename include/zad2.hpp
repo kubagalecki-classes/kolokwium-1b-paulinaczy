969e9b1ffbefdd30d5bfd2c21a6b96f800fac1ac
paulinaczy <73669289+paulinaczy@users.noreply.github.com>
2021-01-15 09:51:02 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -5,3 +5,28 @@
 #include <cctype>
 
 // tutaj klasy PopGwiazda, RapGwiazda oraz funkcja stworzArtyste
+class PopGwiazda: public Artysta {
+public:
+	PopGwiazda(const string& a, const unsigned& b):pseudonim(a), licz_sl(b){}
+	virtual void graj(ostream& stream) const override {
+		stream << "Pop Gwiazda" << pseudonim;
+	}
+private:
+	string pseudonim;
+	unsigned licz_sl;
+};
+
+class RapGwiazda : public Artysta {
+public:
+	RapGwiazda(const string& c, const unsigned& d):pseudonim(c), licz_sl(d){}
+	virtual void graj(ostream& stream) const override {
+		stream << "Rap Gwiazda" << pseudonim;
+	}
+private:
+	string pseudonim;
+	unsigned licz_sl;
+};
+
+Artysta* stworzArtyste(string e) {
+
+}
