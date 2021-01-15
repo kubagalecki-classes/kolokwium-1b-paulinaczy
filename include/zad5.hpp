7b09331ce11ac2cd71ad33545d70f1fe70c4b78b
paulinaczy <73669289+paulinaczy@users.noreply.github.com>
2021-01-15 09:58:35 +0100

    Update zad5.hpp

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -5,3 +5,10 @@
 #include <vector>
 
 // tutaj algorytm liczPopularnych(T begin, T end)
+template <typename T>
+int liczPopularnych(T iter, T ite) {
+	return count_if(iter, ite, [](Artysta h) {
+		return h.getSluchacze() > 1000;
+	}
+	)
+}
