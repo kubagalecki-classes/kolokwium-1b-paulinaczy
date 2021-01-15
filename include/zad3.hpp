f0a5028c1d6127e4dd54185ca87ccae2be261bed
paulinaczy <73669289+paulinaczy@users.noreply.github.com>
2021-01-15 09:48:08 +0100

    Update zad3.hpp

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -3,3 +3,14 @@
 #include "zad1.hpp"
 
 //tutaj szablon ArtystaPlusInstrument
+template <class T>
+class ArtystaPlusInstrument {
+public:
+	ArtystaPlusInstrument(const Artysta& i, const T& j) : a(i), i(j) {}
+	unsigned koncert() {
+		i.liczbaUczestnikow(a);
+	}
+private:
+	Artysta a;
+	T i;
+};
